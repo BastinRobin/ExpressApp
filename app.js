1b6a94d790a4a017a34ce344506cd1ceb81e12dc
Bastin Robin <bastinrobins@gmail.com>
2013-12-19 16:14:02 +0530

    Created server.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-/**
+/*
  * Module dependencies.
  */
 
@@ -15,7 +15,6 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -58,8 +57,9 @@ app.get('/drop', function(req, res) {
 });
 
 app.post('/load', function(req, res) {
-	console.log(req);
+	res.send(req.query);
 })
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
